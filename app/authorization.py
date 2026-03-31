9ae427472dadf4dbbcb235c93b9e9a8dd903ff0b
kamil77778890 <118108636+kamil77890@users.noreply.github.com>
2026-03-31 19:38:00 +0200

    authentification fix

--- app/authorization.py
+++ app/authorization.py
@@ -1,13 +1,8 @@
-from flask import request, jsonify
 from functools import wraps
 
 
 def login_decorator(endpoint):
     @wraps(endpoint)
     def wrapper(*args, **kwargs):
-        token = request.headers.get("token")
-        if token == "cos":
-            return endpoint(*args, **kwargs)
-        else:
-            return jsonify({"error": "Brak dostępu"}), 403
+        return endpoint(*args, **kwargs)
     return wrapper
